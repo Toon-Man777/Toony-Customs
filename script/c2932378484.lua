531bad52e77e0c640bb6b7848e5a7032e39bdcd4
Toon-Man777 <jadenjadams06@gmail.com>
2026-04-22 21:57:48 -0500

    TO INFINITY!

--- script/c2932378484.lua
+++ script/c2932378484.lua
@@ -1,24 +1,30 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Continuous ATK/DEF Boost
+	-- Activation
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+
+	-- All LIGHT Machine monsters you control gain 500 ATK/DEF
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(aux.TargetBoolFunction(s.filter))
+	e1:SetTarget(aux.TargetBoolFunction(s.atkfilter))
 	e1:SetValue(500)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e2)
 
-	-- Draw 1 when a LIGHT Machine is destroyed or banished
+	-- When a LIGHT Machine you control is destroyed or banished, draw 1 card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_DRAW)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1,id)
@@ -26,17 +32,18 @@ function s.initial_effect(c)
 	e3:SetTarget(s.drtg)
 	e3:SetOperation(s.drop)
 	c:RegisterEffect(e3)
+	-- Same trigger for banishment
 	local e4=e3:Clone()
 	e4:SetCode(EVENT_REMOVE)
 	c:RegisterEffect(e4)
 end
 
--- Filter for LIGHT Machine monsters
-function s.filter(c)
+-- Filter for the ATK/DEF boost
+function s.atkfilter(c)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_MACHINE)
 end
 
--- Condition: Check if any LIGHT Machine you control was destroyed/banished
+-- Check if a LIGHT Machine was destroyed/banished from your field
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.drfilter,1,nil,tp)
 end
@@ -45,7 +52,7 @@ function s.drfilter(c,tp)
 		and c:IsPreviousAttributeOnField(ATTRIBUTE_LIGHT) and c:IsPreviousRaceOnField(RACE_MACHINE)
 end
 
--- Target/Operation: Draw 1 card
+-- Draw 1 card logic
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
