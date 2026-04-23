0bdfe75e86343c9e047824e14b74d040deddd39a
Toon-Man777 <jadenjadams06@gmail.com>
2026-04-22 22:15:11 -0500

    SPace~

--- script/c2932378482.lua
+++ script/c2932378482.lua
@@ -1,46 +1,42 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Fusion Materials: 2 "Satellite" monsters
+	-- Fusion Materials: 3 Level 5 LIGHT Machine Monsters
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x930),aux.FilterBoolFunctionEx(Card.IsSetCard,0x930))
-	
-	-- Protection from Level 9/Rank 7/Link 3 or lower
+	Fusion.AddProcMixN(c,true,true,s.matfilter,3)
+
+	-- Protection: Cannot be destroyed by Level/Rank 9 or lower, or Link 3 or lower
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e1:SetValue(s.indval)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BY_BATTLE)
+	e1:SetValue(s.indes)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e2:SetValue(s.indval2)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BY_EFFECT)
 	c:RegisterEffect(e2)
 
-	-- Standby Phase: Gain 3000 ATK
+	-- Standby Phase: Gains 3000 ATK
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_MZONE)
 	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 
-	-- Reset ATK to 1000 after attacking
+	-- Atk becomes 1000 at end of damage step
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_ATKCHANGE)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_DAMAGE_STEP_END)
-	e4:SetCondition(s.atkcon2)
-	e4:SetOperation(s.atkop2)
+	e4:SetOperation(s.atkreset)
 	c:RegisterEffect(e4)
 
-	-- Float into materials on destruction
+	-- On destruction: Special Summon materials from GY
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetProperty(EFFECT_FLAG_DELAY)
@@ -51,20 +47,20 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 
-s.listed_series={0x930}
-
-function s.indval(e,c)
-	if not c then return false end
-	local lv,rk,lk=c:GetLevel(),c:GetRank(),c:GetLink()
-	return (lv>0 and lv<=9) or (rk>0 and rk<=7) or (lk>0 and lk<=3)
-end
-function s.indval2(e,re,rp)
-	if not re:IsActiveType(TYPE_MONSTER) then return false end
-	local c=re:GetHandler()
-	local lv,rk,lk=c:GetLevel(),c:GetRank(),c:GetLink()
-	return (lv>0 and lv<=9) or (rk>0 and rk<=7) or (lk>0 and lk<=3)
+-- Filter for Fusion Materials
+function s.matfilter(c,fc,sumtype,tp)
+	return c:IsLevel(5) and c:IsAttribute(ATTRIBUTE_LIGHT,fc,sumtype,tp) and c:IsRace(RACE_MACHINE,fc,sumtype,tp)
 end
 
+-- Battle/Effect Protection Logic
+function s.indes(e,c)
+	local lv=c:GetLevel()
+	local rk=c:GetRank()
+	local lk=c:GetLink()
+	return (lv>0 and lv<=9) or (rk>0 and rk<=9) or (lk>0 and lk<=3)
+end
+
+-- ATK Gain Logic
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
@@ -77,12 +73,10 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.atkcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler()==Duel.GetAttacker()
-end
-function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
+-- ATK Reset Logic
+function s.atkreset(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
+	if c:IsRelateToBattle() and c:IsFaceup() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
@@ -92,21 +86,28 @@ function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+-- Floating Effect Logic
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local mg=e:GetHandler():GetMaterial()
-	if chk==0 then return #mg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#mg 
-		and mg:FilterCount(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false)==#mg end
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>=#mg and #mg>0 
+		and mg:FilterCount(aux.NecroValleyFilter(Card.IsCanBeSpecialSummoned),nil,e,0,tp,false,false)==#mg end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,mg,#mg,0,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local mg=e:GetHandler():GetMaterial()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<#mg then return end
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<#mg then return end
 	for tc in aux.Next(mg) do
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-			local e1=Effect.CreateEffect(e:GetHandler())
+			-- Negate effects until Standby Phase
+			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY)
 			tc:RegisterEffect(e1)
 			local e2=e1:Clone()
 			e2:SetCode(EFFECT_DISABLE_EFFECT)
