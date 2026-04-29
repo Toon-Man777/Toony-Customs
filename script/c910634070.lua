2df920faa90d528d2756d2c6b50558683112fc58
Toon-Man777 <jadenjadams06@gmail.com>
2026-04-29 18:42:53 -0500

    cgf

--- script/c910634070.lua
+++ script/c910634070.lua
@@ -1,41 +1,53 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Hand SS + Mill 2
+	-- 1. Special Summon from hand + Mill 2
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(0x1+0x4000) 
-	e1:SetType(4) 
-	e1:SetRange(2) 
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(0x1+0x4000) -- CATEGORY_SPECIAL_SUMMON + CATEGORY_DECKDES
+	e1:SetType(4) -- EFFECT_TYPE_IGNITION
+	e1:SetRange(2) -- LOCATION_HAND
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	-- Always Tuner
+
+	-- 2. Can be treated as a non-tuner for "Iron Chain" Synchro
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(1)
-	e2:SetCode(4003) 
+	e2:SetType(1) -- EFFECT_TYPE_SINGLE
+	e2:SetProperty(0x400) -- EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE
+	e2:SetCode(4005) -- EFFECT_NONTUNER_S_MATERIAL
+	e2:SetValue(function(e,sc) return sc:IsSetCard(0x25) end)
 	c:RegisterEffect(e2)
-	-- Level +/- 1
+
+	-- 3. Level +/- 1 (Once per turn)
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(4)
-	e3:SetRange(4)
+	e3:SetRange(4) -- LOCATION_MZONE
 	e3:SetCountLimit(1)
 	e3:SetOperation(s.lvop)
 	c:RegisterEffect(e3)
-	-- Reactive SS (Thrice per turn)
+
+	-- 4. Quick Effect: Mill Trigger -> Special Summon
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(0x1)
-	e4:SetType(160)
-	e4:SetCode(4101)
+	e4:SetType(16+0x40) -- EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O
+	e4:SetProperty(0x10000) -- EFFECT_FLAG_DELAY
+	e4:SetCode(1001) -- EVENT_TO_GRAVE
 	e4:SetRange(4)
-	e4:SetCountLimit(3)
+	e4:SetCountLimit(1)
 	e4:SetCondition(s.spcon2)
 	e4:SetTarget(s.sptg2)
 	e4:SetOperation(s.spop2)
 	c:RegisterEffect(e4)
 end
+
+-- Hand SS + Mill Logic
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,4)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,0x1,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,0x4000,nil,0,1-tp,2)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -43,6 +55,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.DiscardDeck(1-tp,2,64)
 	end
 end
+
+-- Level Mod Logic
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
@@ -50,21 +64,24 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		local val=(op==0) and 1 or -1
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(1)
-		e1:SetCode(12)
+		e1:SetCode(12) -- EFFECT_UPDATE_LEVEL
 		e1:SetValue(val)
 		e1:SetReset(0x1fe0000)
 		c:RegisterEffect(e1)
 	end
 end
+
+-- Reactive SS Logic
 function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(function(tc) return tc:IsPreviousLocation(1) and tc:GetControler()==1-tp end,1,nil)
+	-- When a card from opponent's deck is sent to GY
+	return eg:IsExists(function(c) return c:IsControler(1-tp) and c:IsPreviousLocation(1) end,1,nil)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,4)>0 and Duel.IsExistingMatchingCard(function(c,e,tp) return c:IsSetCard(0x25) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,8) end,tp,1+16,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,0x1,nil,1,tp,1+16)
+	if chk==0 then return Duel.GetLocationCount(tp,4)>0 and Duel.IsExistingMatchingCard(function(c,e,tp) return c:IsSetCard(0x25) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,2+16,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,0x1,nil,1,tp,2+16)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(3,tp,509)
-	local g=Duel.SelectMatchingCard(tp,function(c,e,tp) return c:IsSetCard(0x25) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,8) end,tp,1+16,0,1,1,nil,e,tp)
-	if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,8) end
+	local g=Duel.SelectMatchingCard(tp,function(c,e,tp) return c:IsSetCard(0x25) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,2+16,0,1,1,nil,e,tp)
+	if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,1) end
 end
--- script/c910634074.lua
+++ script/c910634074.lua
@@ -2,19 +2,21 @@ local s,id=GetID()
 function s.initial_effect(c)
 	-- Activation: Special Summon 1 Level 4 or lower "Iron Chain"
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(0x1) -- CATEGORY_SPECIAL_SUMMON
 	e1:SetType(0x10000) -- EFFECT_TYPE_ACTIVATE
 	e1:SetCode(0) -- EVENT_FREE_CHAIN
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.acttg)
+	e1:SetOperation(s.actop)
 	c:RegisterEffect(e1)
 
 	-- Main Deck Lock: While controlling 2+ "Iron Chain" monsters
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(16) -- EFFECT_TYPE_FIELD
 	e2:SetCode(460) -- EFFECT_CANNOT_SPECIAL_SUMMON
-	e2:SetRange(8) -- LOCATION_SZONE (Field slot)
+	e2:SetRange(256) -- LOCATION_FZONE (Changed from 8 to 256)
 	e2:SetProperty(0x4000000) -- EFFECT_FLAG_PLAYER_TARGET
-	e2:SetTargetRange(1,1) -- Both players
+	e2:SetTargetRange(1,1) 
 	e2:SetCondition(s.lockcon)
 	e2:SetTarget(s.locktg)
 	c:RegisterEffect(e2)
@@ -23,8 +25,8 @@ function s.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(16)
 	e3:SetCode(100) -- EFFECT_UPDATE_ATTACK
-	e3:SetRange(8)
-	e3:SetTargetRange(4,0) -- Your monsters
+	e3:SetRange(256) -- LOCATION_FZONE
+	e3:SetTargetRange(4,0) 
 	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x25))
 	e3:SetValue(800)
 	c:RegisterEffect(e3)
@@ -32,39 +34,40 @@ function s.initial_effect(c)
 	e4:SetCode(101) -- EFFECT_UPDATE_DEFENSE
 	c:RegisterEffect(e4)
 
-	-- Banish Mill Replacement
+	-- Banish Redirect (Mill replacement)
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(16)
-	e5:SetCode(133) -- EFFECT_BUREAU_REDIRECT (Redirecting to Grave)
-	e5:SetProperty(0x800000) -- EFFECT_FLAG_SET_AVAILABLE
-	e5:SetRange(8)
+	e5:SetCode(133) -- EFFECT_TO_GRAVE_REDIRECT
+	e5:SetRange(256) -- LOCATION_FZONE
 	e5:SetTargetRange(0,1) -- Affects opponent's cards
 	e5:SetTarget(s.rdtg)
 	e5:SetValue(32) -- LOCATION_REMOVED
 	c:RegisterEffect(e5)
 end
 
--- Activation SS Logic
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+-- Activation Logic
+function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,0x1,nil,1,tp,1)
+end
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(function(c,e,tp) return c:IsSetCard(0x25) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end,tp,1,0,nil,e,tp)
 	if #g>0 and Duel.GetLocationCount(tp,4)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(3,tp,509)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,1)
 	end
 end
 
--- Special Summon Lock Logic
+-- Lock Logic
 function s.lockcon(e)
-	-- Requirement: Control 2+ Iron Chains
 	return Duel.IsExistingMatchingCard(Card.IsSetCard,e:GetHandlerPlayer(),4,0,2,nil,0x25)
 end
 function s.locktg(e,c,sump,sumtype,sumpos,targetp,se)
-	-- Blocks SS from Main Deck (Location 1) except Iron Chains
 	return c:IsLocation(1) and not c:IsSetCard(0x25)
 end
 
--- Banish Redirect Logic (Mill replacement)
+-- Redirect Logic
 function s.rdtg(e,c)
-	-- Triggered if a card from the opponent's Deck is headed to the GY
 	return c:IsLocation(1) and c:GetOwner()~=e:GetHandlerPlayer()
 end
