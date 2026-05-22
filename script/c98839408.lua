7c9a8a561207f0be11e47bd6c116d336f6b8943d
Toon-Man777 <jadenjadams06@gmail.com>
2026-05-22 01:05:00 -0500

    cvbnm,

--- script/c98839408.lua
+++ script/c98839408.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	e0:SetValue(0x2f) -- Ice Barrier Archetype Hex
 	c:RegisterEffect(e0)
 
-	-- 1. When Synchro Summoned: Add 1 "Cursed" Spell Card from Deck to Hand
+	-- 1. When Synchro Summoned: Add 1 "Cursed" card from Deck to Hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -25,7 +25,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 
-	-- 2. Twice per turn Quick Effect: Negate opponent's card/effect activation and SS Shakespeare
+	-- 2. Quick Effect: Negate activation and SS a clone from Extra Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON)
@@ -33,33 +33,32 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_BUFF)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(2) -- Twice per turn restriction
+	e2:SetCountLimit(1,id+1) -- Hard once per turn rule applied to this effect tracking
 	e2:SetCondition(s.negcon)
 	e2:SetTarget(s.negtg)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
 
-	-- 3. Banish "Cursed Star Crimson Eclipse" to grant effects to Shakespeare clones
+	-- 3. Banish generic Spells from GY to buff WATER Fiend Synchros
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,id+1)
+	e3:SetCountLimit(1,id+2)
 	e3:SetCost(s.effcost)
 	e3:SetOperation(s.effop)
 	c:RegisterEffect(e3)
 end
 
--- Name variables for custom card string tracking
+-- Name constant for Extra Deck clone tracking
 s.shake_name = "Cursed With Pride, Frozen Jokester Shakespeare"
-s.eclipse_name = "Cursed Star Crimson Eclipse"
 
--- Effect 1: Search Logic
+-- Effect 1: Archetype-wide search
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x923) and c:IsSpell() and c:IsAbleToHand()
+	return c:IsSetCard(0x923) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -74,128 +73,122 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 2: Negate & Summon Logic
+-- Effect 2: Negate & Extra Deck Summon Engine
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp~=tp and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
 function s.ssfilter(c,e,tp)
-	return c:IsCleanName(s.shake_name) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCleanName(s.shake_name) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.ssfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.ssfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.ssfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.ssfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		Duel.NegateActivation(ev)
 	end
 end
 
--- Effect 3: Cost and Dynamic Buff Logic
+-- Effect 3: Banish Spells for Tiered Combat Traits
 function s.costfilter(c)
-	return c:IsCleanName(s.eclipse_name) and c:IsAbleToBanishAsCost()
+	return c:IsSpell() and c:IsAbleToRemoveAsCost() -- Bug fix fixated: Native removal verification rule
 end
 function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,3,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-	e:SetLabel(#g) -- Store total count of banished items
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,99,nil)
+	local count=Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabel(count)
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local count=e:GetLabel()
 	if count==0 then return end
 
-	-- Register a dynamic lingering aura rule onto the field matching your Shakespeare instances
+	-- Aura filter targeting rule: Face-up WATER Fiend Synchro Monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(s.shaketg)
+	e1:SetTarget(s.synchrofilter)
 	e1:SetValue(s.atkval)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 
-	-- 1+ Banished: No battle damage involving them
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.shaketg)
-	e2:SetValue(1)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-
-	-- 2+ Banished updates
+	-- 2+ Banished items checked
 	if count >= 2 then
-		-- Double Attack
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_FIELD)
-		e3:SetCode(EFFECT_EXTRA_ATTACK)
-		e3:SetTargetRange(LOCATION_MZONE,0)
-		e3:SetTarget(s.shaketg)
-		e3:SetValue(1)
-		e3:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e3,tp)
+		-- Attack twice per turn
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_EXTRA_ATTACK)
+		e2:SetTargetRange(LOCATION_MZONE,0)
+		e2:SetTarget(s.synchrofilter)
+		e2:SetValue(1)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
 
 		-- Banish monsters destroyed by battle
-		local e4=Effect.CreateEffect(c)
-		e4:SetType(EFFECT_TYPE_FIELD)
-		e4:SetCode(EFFECT_BATTLE_DESTROY_REDIRECT)
-		e4:SetTargetRange(LOCATION_MZONE,0)
-		e4:SetTarget(s.shaketg)
-		e4:SetValue(LOCATION_REMOVED)
-		e4:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e4,tp)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD)
+		e3:SetCode(EFFECT_BATTLE_DESTROY_REDIRECT)
+		e3:SetTargetRange(LOCATION_MZONE,0)
+		e3:SetTarget(s.synchrofilter)
+		e3:SetValue(LOCATION_REMOVED)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e3,tp)
 	end
 
-	-- 3 Banished updates
-	if count == 3 then
+	-- 3+ Banished items checked
+	if count >= 3 then
 		-- Unaffected by opponent's card effects
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_FIELD)
+		e4:SetCode(EFFECT_IMMUNE_EFFECT)
+		e4:SetTargetRange(LOCATION_MZONE,0)
+		e4:SetTarget(s.synchrofilter)
+		e4:SetValue(s.immunefilter)
+		e4:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e4,tp)
+
+		-- Target battle destruction hook to trigger card effect negation
 		local e5=Effect.CreateEffect(c)
-		e5:SetType(EFFECT_TYPE_FIELD)
-		e5:SetCode(EFFECT_IMMUNE_EFFECT)
-		e5:SetTargetRange(LOCATION_MZONE,0)
-		e5:SetTarget(s.shaketg)
-		e5:SetValue(s.immunefilter)
+		e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e5:SetCode(EVENT_BATTLE_DESTROYING)
+		e5:SetCondition(s.negatetrcon)
+		e5:SetOperation(s.negateterop)
 		e5:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e5,tp)
-
-		-- Trigger hook: If a Shakespeare kills something by battle, negate 1 opponent card
-		local e6=Effect.CreateEffect(c)
-		e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e6:SetCode(EVENT_BATTLE_DESTROYING)
-		e6:SetCondition(s.negatetrcon)
-		e6:SetOperation(s.negateterop)
-		e6:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e6,tp)
 	end
 end
 
--- Helper matching hooks
-function s.shaketg(e,c)
-	return c:IsFaceup() and c:IsCleanName(s.shake_name)
+-- Target checking filters
+function s.synchrofilter(e,c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_FIEND) and c:IsType(TYPE_SYNCHRO)
 end
 function s.atkval(e,c)
 	local tp=e:GetHandlerPlayer()
-	local g=Duel.GetMatchingGroup(s.shaketg,tp,LOCATION_MZONE,0,nil)
-	return #g * 1000
+	-- Dynamic loop matching checking duplicate names face-up on field
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local count=0
+	for tc in aux.Next(g) do
+		if tc:IsCleanName(c:GetCleanName()) then
+			count = count + 1
+		end
+	end
+	return count * 1000
 end
 function s.immunefilter(e,re)
 	return re:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
 
--- Option 3 trigger negation handling
+-- Tier 3 continuous operational loops
 function s.negatetrcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
-	return tc and tc:IsControler(tp) and s.shaketg(e,tc) and tc:IsRelateToBattle()
+	return tc and tc:IsControler(tp) and s.synchrofilter(e,tc) and tc:IsRelateToBattle()
 end
 function s.negateterop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsExistingMatchingCard(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) 
