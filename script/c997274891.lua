600afd123b2a6b39201aa9b707feedbee397208e
Toon-Man777 <jadenjadams06@gmail.com>
2026-06-05 01:43:50 -0500

    jhgjm hm jm jhgjgm bch,dfjjdbbjnyt kvvfggfh.jk

--- script/c997274891.lua
+++ script/c997274891.lua
@@ -1,93 +1,84 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- 1. Activate: Add 1 Union monster from Deck, then optionally add a monster it mentions
+	-- Activate: Union Roll Call
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-
-	-- 2. GY Effect: Banish this card to target 1 monster you control; equip it with an appropriate Union from Hand/GY
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_EQUIP)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id+100)
-	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(s.eqtg)
-	e2:SetOperation(s.eqop)
-	c:RegisterEffect(e2)
 end
 
--- 1. Main Search Logic
-function s.thfilter(c)
-	return c:IsType(TYPE_UNION) and c:IsAbleToHand()
+-- Filter to find legal face-up targets on your field
+function s.tgfilter(c,tp)
+	return c:IsFaceup() and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_GRAVE,0,1,nil,c,tp)
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+
+-- FIXED: Replaced old CheckUnionEquip function with a modern aux.UnionFilter setup
+function s.eqfilter(c,tc,tp)
+	return c:IsType(TYPE_UNION) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and aux.UnionFilter(c,tc)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_HAND) then
-		local tc=g:GetFirst()
-		Duel.ConfirmCards(1-tp,tc)
-		
-		-- Check if the added Union monster lists specific monster names
-		if tc.listed_names and #tc.listed_names > 0 then
-			-- Filter names mentioned by the card that exist in the deck and can be added to hand
-			local name_filter = function(c)
-				if not c:IsAbleToHand() then return false end
-				for _, name in ipairs(tc.listed_names) do
-					if c:IsCode(name) then return true end
-				end
-				return false
-			end
-			
-			if Duel.IsExistingMatchingCard(name_filter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-				Duel.BreakEffect()
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-				local sg=Duel.SelectMatchingCard(tp,name_filter,tp,LOCATION_DECK,0,1,1,nil)
-				if #sg>0 then
-					Duel.SendtoHand(sg,nil,REASON_EFFECT)
-					Duel.ConfirmCards(1-tp,sg)
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
+	-- Union Roll Call lets you target face-up monsters you control up to your available zones
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return false end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUE_EYES_SPIRIT) then ft=1 end
+	
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,ft,nil,tp)
+	
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,#g,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,#g,tp,LOCATION_GRAVE)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetTargetCards(e)
+	if #g==0 then return end
+	
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUE_EYES_SPIRIT) then ft=1 end
+	
+	-- Only process monsters that can still have a Union monster equipped to them
+	local tg=g:Filter(Card.IsFaceup,nil)
+	if #tg==0 then return end
+	
+	local count=math.min(#tg, ft)
+	local summoned=Group.CreateGroup()
+	
+	for tc in aux.Next(tg) do
+		if count>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local eqg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.eqfilter),tp,LOCATION_GRAVE,0,1,1,nil,tc,tp)
+			if #eqg>0 then
+				local eqc=eqg:GetFirst()
+				if Duel.SpecialSummonStep(eqc,0,tp,tp,false,false,POS_FACEUP) then
+					-- Track successful summons to equip them afterward
+					eqc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,tc:GetFieldID())
+					summoned:AddCard(eqc)
+					count=count-1
 				end
 			end
 		end
 	end
-end
-
--- 2. GY Equip Logic
-function s.eqfilter(c,tc,tp)
-	-- Checks if the card is a Union monster and if it can legally equip to the target 'tc'
-	return c:IsType(TYPE_UNION) and c:CheckUnionTarget(tc) and Duel.CheckUnionEquip(c,tc,tp)
-end
-function s.tgfilter(c,tp)
-	return c:IsFaceup() and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,c,tp)
-end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
-end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	local tc=Duel.GetFirstTarget()
-	if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	Duel.SpecialSummonComplete()
 	
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.eqfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,tc,tp)
-	local ec=g:GetFirst()
-	if ec then
-		-- Perform the native system Union equipment registration sequence
-		Duel.EquipToMonster(tp,ec,tc)
+	-- Perform the forced Union Equipment logic
+	for eqc in aux.Next(summoned) do
+		local fid=eqc:GetFlagEffectLabel(id)
+		local tc=tg:Filter(function(c) return c:GetFieldID()==fid end, nil):GetFirst()
+		if tc and tc:IsFaceup() and eqc:IsLocation(LOCATION_MZONE) then
+			-- Equips the newly summoned Union monster directly out of the monster zone to its target
+			Duel.Equip(tp,eqc,tc)
+			aux.SetUnionState(eqc)
+		end
 	end
 end
--- script/c997274892.lua
+++ script/c997274892.lua
@@ -1,6 +1,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- 1. Activate: Fusion Summon 1 Fusion Monster using Union monsters from Hand/Deck
+	-- 1. Activate: Fusion Summon using Union monsters from Hand or DECK
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -9,94 +9,77 @@ function s.initial_effect(c)
 	e1:SetOperation(s.fusop)
 	c:RegisterEffect(e1)
 
-	-- 2. GY Effect: Banish this card to target 1 banished Union monster; add it to hand
+	-- 2. GY Effect: Banish this card to add 1 banished Union monster to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id) -- "You can only use this effect of 'Hanger Polymerization' once per turn."
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id) -- "You can only use this effect of 'Hangar Polymerization' once per turn"
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
 
--- 1. Custom Hand/Deck Fusion Mechanics
+-- Filter: Materials must be Union monsters
 function s.ffilter(c)
 	return c:IsType(TYPE_UNION)
 end
-function s.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+
+-- Check if the Fusion Monster can be summoned using our specific rules
+function s.fusfilter(c,e,tp,mg)
+	return c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+		and c:CheckFusionMaterial(mg,nil,chkf)
 end
+
+-- 1. Fusion Activation Target
 function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=tp
-		local mg1=Duel.GetMatchingGroup(s.ffilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
-		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg2=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
-			end
-		end
+		-- Gather available materials from Hand and DECK that are Union monsters
+		local mg=Duel.GetMatchingGroup(s.ffilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
+		local res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg)
 		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
+
+-- 1. Fusion Activation Operation
 function s.fusop(e,tp,eg,ep,ev,re,r,rp)
-	-- Restriction Lock applies immediately upon activation resolution
+	local chkf=tp
+	-- Lock player into Union monsters for the rest of the turn
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,2)) -- Displays lock UI to player
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 
-	local chkf=tp
-	local mg1=Duel.GetMatchingGroup(s.ffilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
-	local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if #sg1>0 or (sg2~=nil and #sg2>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (mg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
+	-- Resolve Fusion Summon
+	local mg=Duel.GetMatchingGroup(s.ffilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,mg)
+	local tc=g:GetFirst()
+	if tc then
+		local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
+		tc:SetMaterial(mat)
+		Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+		Duel.BreakEffect()
+		Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
 		tc:CompleteProcedure()
 	end
 end
-function s.splimit(e,c)
+
+-- Restrict special summons to ONLY Union monsters
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsType(TYPE_UNION)
 end
 
--- 2. GY Recycle Logic
+-- 2. GY Salvage Target
 function s.thfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_UNION) and c:IsAbleToHand()
 end
@@ -107,10 +90,11 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
+
+-- 2. GY Salvage Operation
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
 	end
 end
