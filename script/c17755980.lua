82fa8655a184ee399cb8f32e0bf4bc1b59a5de71
Toon-Man777 <jadenjadams06@gmail.com>
2026-06-11 09:40:37 -0500

    ghjk

--- script/c17755980.lua
+++ script/c17755980.lua
@@ -4,178 +4,181 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT),8,3)
 	c:EnableReviveLimit()
 
-	-- Rule: Can only control 1 "The One Refracted By Zero, Galaxy-Eyes Cipher Dragon"
+	-- Condition: Can only control 1 "The One Refracted By Zero, Galaxy-Eyes Cipher Dragon"
 	c:SetUniqueOnField(1,0,id)
 
-	-- Continuous: Unaffected by opponent's card effects while holding "Galaxy-Eyes Photon Dragon"
+	-- Effect 1: Unaffected by opponent's card effects if it has "Galaxy-Eyes Photon Dragon" as material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.imm_cond)
-	e1:SetValue(s.imm_val)
+	e1:SetCondition(s.immcon)
+	e1:SetValue(s.efilter)
 	c:RegisterEffect(e1)
 
-	-- Effect 1: When Xyz Summoned, search 1 "Galaxy", "Photon", or "Cipher" monster, AND Special Summon 1 LIGHT monster from hand
+	-- Effect 2: When Xyz Summoned: Search 1 "Galaxy", "Photon", or "Cipher" monster AND Special Summon 1 LIGHT monster from hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	-- FIXED: Changed from EVENT_SXYZ_SUMMON to EVENT_SPSUMMON to fix the nil 'SetCode' crash
-	e2:SetCode(EVENT_SPSUMMON)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id) -- Hard once per turn
-	e2:SetCondition(s.th_sp_cond) -- Conditions it strictly to Xyz Summons
-	e2:SetTarget(s.th_sp_tg)
-	e2:SetOperation(s.th_sp_op)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 
-	-- Effect 2: Quick Effect battle step banish trick, can attack a second time, returns target at end of Battle Phase
+	-- Effect 3: Quick Effect (Battle Step): Detach 1, banish opponent's battling monster, make a second attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetHintTiming(TIMING_BATTLE_STEP)
-	e3:SetCountLimit(1,id+100) -- Hard once per turn
-	e3:SetCondition(s.ban_cond)
-	e3:SetCost(aux.xyzcost)
-	e3:SetTarget(s.ban_tg)
-	e3:SetOperation(s.ban_op)
+	e3:SetCountLimit(1,id+100)
+	-- Line 41 Fix: Standardizing both inputs to integers to solve the parameter 2 nil crash
+	e3:SetHintTiming(TIMING_BATTLE_PHASE,TIMING_BATTLE_PHASE)
+	e3:SetCondition(s.bancon)
+	e3:SetCost(s.bancost)
+	e3:SetTarget(s.bantg)
+	e3:SetOperation(s.banop)
 	c:RegisterEffect(e3)
 
-	-- Effect 3: End Phase banish Xyz Monsters from GY to gain 500 ATK per Xyz Monster in banished zone
+	-- Effect 4: End Phase: Banish any number of Xyz Monsters from GY to gain 500 ATK for each card in banished zone
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,id+200) -- Hard once per turn
-	e4:SetTarget(s.atk_tg)
-	e4:SetOperation(s.atk_op)
+	e4:SetCountLimit(1,id+200)
+	e4:SetTarget(s.atkcon_tg)
+	e4:SetOperation(s.atkcon_op)
 	c:RegisterEffect(e4)
 end
 
-s.listed_names={93717133} -- "Galaxy-Eyes Photon Dragon"
-s.listed_series={0x7b,0x93,0xe1} -- Galaxy (0x7b), Photon (0x93), Cipher (0xe1)
+s.listed_names={93717133} -- Galaxy-Eyes Photon Dragon ID
 
--- Immunity condition check (requires "Galaxy-Eyes Photon Dragon" as material)
-function s.imm_cond(e)
+-- Effect 1 Handlers (Immunity)
+function s.immcon(e)
 	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,93717133)
 end
-function s.imm_val(e,te)
+function s.efilter(e,te)
 	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
 
--- Condition to make sure it was Xyz Summoned safely
-function s.th_sp_cond(e,tp,eg,ep,ev,re,r,rp)
+-- Effect 2 Handlers (Search + Special Summon)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
-
--- Filter for searching "Galaxy", "Photon", or "Cipher" monsters
-function s.search_filter(c)
-	return c:IsMonster() and (c:IsSetCard(0x7b) or c:IsSetCard(0x93) or c:IsSetCard(0xe1)) and c:IsAbleToHand()
+function s.filter(c)
+	return c:IsMonster() and (c:IsSetCard(0x7b) or c:IsSetCard(0x55) or c:IsSetCard(0xe1)) and c:IsAbleToHand()
 end
--- Filter for summoning LIGHT monsters from hand
-function s.sp_filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)
 end
-
--- Effect 1 Handlers (Search + Special Summon)
-function s.th_sp_tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.search_filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_HAND)
 end
-function s.th_sp_op(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.search_filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 and g:GetFirst():IsLocation(LOCATION_HAND) then
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_HAND) then
 		Duel.ConfirmCards(1-tp,g)
-		-- Then special summon 1 LIGHT monster from hand in defense position
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.sp_filter,tp,LOCATION_HAND,0,1,nil,e,tp) then
-			if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-				Duel.BreakEffect()
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-				local sg=Duel.SelectMatchingCard(tp,s.sp_filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-				if #sg>0 then
-					Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_DEFENSE)
-				end
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+			if #sg>0 then
+				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_DEFENSE)
 			end
 		end
 	end
 end
 
--- Effect 2 Handlers (Battle Step Banish and Second Attack)
-function s.ban_cond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()==PHASE_BATTLE_STEP and Duel.GetAttacker() and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler())
+-- Effect 3 Handlers (Battle Step Intercept Banish)
+function s.bancon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return Duel.IsBattlePhase() and bc and bc:IsControler(1-tp) and bc:IsRelateToBattle()
 end
-function s.ban_tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local bc=e:GetHandler():GetBattleMonster()
-	if chk==0 then return bc and bc:IsControler(1-tp) and bc:IsAbleToRemove() end
+function s.bancost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return bc and bc:IsAbleToRemove() end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
 end
-function s.ban_op(e,tp,eg,ep,ev,re,r,rp)
+function s.banop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local bc=c:GetBattleMonster()
-	if bc and bc:IsControler(1-tp) and Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)~=0 then
-		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetCode(EFFECT_EXTRA_ATTACK)
-			e1:SetValue(1)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
-			c:RegisterEffect(e1)
-		end
-		if bc:IsLocation(LOCATION_REMOVED) then
+	local bc=c:GetBattleTarget()
+	if bc and bc:IsRelateToBattle() and Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)>0 then
+		-- Return to field tracking event setup
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+		e1:SetCountLimit(1)
+		e1:SetLabelObject(bc)
+		e1:SetCondition(s.retcon)
+		e1:SetOperation(s.retop)
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+		Duel.RegisterEffect(e1,tp)
+		
+		-- Gain second attack capability
+		if c:IsRelateToEffect(e) then
 			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
-			e2:SetCountLimit(1)
-			e2:SetLabelObject(bc)
-			e2:SetCondition(s.ret_cond)
-			e2:SetOperation(s.ret_op)
-			e2:SetReset(RESET_PHASE+PHASE_BATTLE)
-			Duel.RegisterEffect(e2,tp)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e2:SetCode(EFFECT_EXTRA_ATTACK)
+			e2:SetValue(1)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+			c:RegisterEffect(e2)
 		end
 	end
 end
-function s.ret_cond(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():IsLocation(LOCATION_REMOVED)
-end
-function s.ret_op(e,tp,eg,ep,ev,re,r,rp)
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
-	Duel.ReturnToField(tc)
+	return tc and tc:GetReasonEffect():GetHandler()==e:GetOwner()
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc then
+		Duel.ReturnToField(tc)
+	end
 end
 
--- Effect 3 Handlers (End Phase GY Banish to gain ATK)
-function s.atk_gy_filter(c)
-	return c:IsType(TYPE_XYZ) and c:IsMonster() and c:IsAbleToRemove()
+-- Effect 4 Handlers (End Phase Steer)
+function s.xyzgyfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsAbleToRemove()
 end
-function s.atk_ban_filter(c)
-	return c:IsType(TYPE_XYZ) and c:IsMonster() and c:IsLocation(LOCATION_REMOVED)
-end
-function s.atk_tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.atk_gy_filter,tp,LOCATION_GRAVE,0,1,nil) end
+function s.atkcon_tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzgyfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
 end
-function s.atk_op(e,tp,eg,ep,ev,re,r,rp)
+function s.atkcon_op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.atk_gy_filter,tp,LOCATION_GRAVE,0,1,60,nil)
-	if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
-		local count=Duel.GetMatchingGroupCount(s.atk_ban_filter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)
-		if count>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(count*500)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-			c:RegisterEffect(e1)
+	local g=Duel.SelectMatchingCard(tp,s.xyzgyfilter,tp,LOCATION_GRAVE,0,1,99,nil)
+	if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			-- Calculate completely based on absolute size of face-up banished zone items
+			local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)
+			if ct>0 then
+				Duel.BreakEffect()
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetValue(ct*500)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+				c:RegisterEffect(e1)
+			end
 		end
 	end
 end
