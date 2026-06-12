91a291671412ff20668d4fda16a6acf7d8d58401
Toon-Man777 <jadenjadams06@gmail.com>
2026-06-11 22:13:15 -0500

    dd

--- script/c17755980.lua
+++ script/c17755980.lua
@@ -30,30 +30,26 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 
-	-- Effect 3: Quick Effect (Battle Step): Detach 1, banish opponent's battling monster, make a second attack
+	-- Effect 3: If this card battles an opponent's monster: Banish it, then make a second attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_START)
 	e3:SetCountLimit(1,id+100) -- Hard Once Per Turn
-	-- Universal 2-integer bitmask to prevent parameter 2 "nil" crashes completely
-	e3:SetHintTiming(0,TIMING_BATTLE_PHASE)
 	e3:SetCondition(s.bancon)
-	e3:SetCost(s.bancost)
 	e3:SetTarget(s.bantg)
 	e3:SetOperation(s.banop)
 	c:RegisterEffect(e3)
 
-	-- Effect 4: End Phase: Banish any number of Xyz Monsters from GY to gain 500 ATK for each Xyz monster in the banished zone
+	-- Effect 4: Ignition Effect: Detach 1, banish any number of Xyz Monsters from GY to gain 500 ATK per banished monster
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1,id+200) -- Hard Once Per Turn
+	e4:SetCost(s.atkcost)
 	e4:SetTarget(s.atktg)
 	e4:SetOperation(s.atkop)
 	c:RegisterEffect(e4)
@@ -100,16 +96,10 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 3 (Battle Step Intercept Banish)
+-- Effect 3 (Banish on battle start)
 function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local bc=c:GetBattleTarget()
-	return Duel.GetCurrentPhase()==PHASE_BATTLE and Duel.GetCurrentChain()==0
-		and bc and bc:IsControler(1-tp) and bc:IsRelateToBattle()
-end
-function s.bancost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
 end
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local bc=e:GetHandler():GetBattleTarget()
@@ -121,7 +111,7 @@ function s.banop(e,tp,eg,ep,ev,re,r,rp)
 	local bc=c:GetBattleTarget()
 	if bc and bc:IsRelateToBattle() and Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)>0 then
 		if bc:IsLocation(LOCATION_REMOVED) then
-			-- System hook to safely return the monster at the end of the Battle Phase
+			-- Delays return cleanly until the end of the Battle Phase
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 			e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
@@ -132,7 +122,7 @@ function s.banop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_PHASE+PHASE_BATTLE)
 			Duel.RegisterEffect(e1,tp)
 		end
-		-- Grant a second attack
+		-- Gain second attack
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
@@ -155,12 +145,13 @@ function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
--- Effect 4 (End Phase Multi-Banish ATK Booster)
+-- Effect 4 (Ignition ATK Boost)
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
 function s.xyzgyfilter(c)
 	return c:IsType(TYPE_XYZ) and c:IsAbleToRemove()
-end
-function s.xyzbanfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzgyfilter,tp,LOCATION_GRAVE,0,1,nil) end
@@ -172,10 +163,9 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.xyzgyfilter,tp,LOCATION_GRAVE,0,1,99,nil)
 	if #g>0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			-- Counts total face-up Xyz Monsters across both player's banished zones
-			local ct=Duel.GetMatchingGroupCount(s.xyzbanfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)
+			-- Dynamic calculation counting ALL items currently in the Banished Zone
+			local ct=Duel.GetMatchingGroupCount(nil,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)
 			if ct>0 then
-				Duel.BreakEffect()
 				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
 				e1:SetCode(EFFECT_UPDATE_ATTACK)
